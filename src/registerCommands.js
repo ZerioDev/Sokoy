55a65237556be38a6035c8a5da1a06e387f8855d
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-10 11:51:10 +0200

    🎨 Format commands

--- src/registerCommands.js
+++ src/registerCommands.js
@@ -1,17 +1,26 @@
-const { readdirSync } = require('fs');
 const { Collection } = require('discord.js');
+const { readdirSync } = require('fs');
+
+const commands = [];
+
+function searchFiles() {
+    readdirSync('./commands/').map(dirs => {
+        const files = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+        files.map(cmd => commands.push(require(`../commands/${dirs}/${cmd}`)));
+    });
+}
 
 client.commands = new Collection();
 
-console.log('Loading commands...');
+function buildCommands() {
+    console.log('Loading commands');
 
-readdirSync('./commands/').forEach(dirs => {
-    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+    commands.map(file => {
+        console.log(`Loaded command ${file.name.toLowerCase()}`);
+        client.commands.set(file.name.toLowerCase(), file);
+    });
+}
 
-    for (const file of commands) {
-        const command = require(`../commands/${dirs}/${file}`);
-        console.log(`Loaded command ${command.name.toLowerCase()}`);
-        client.commands.set(command.name.toLowerCase(), command);
-        delete require.cache[require.resolve(`../commands/${dirs}/${file}`)];
-    };
-});
+searchFiles();
+buildCommands();

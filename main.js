002665b0072e71a736cbb5a9390874c3ed1d063f
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-10 11:56:23 +0200

    ✨ Slash commands

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { Client, Intents } = require('discord.js');
 
 global.client = new Client({
@@ -13,4 +15,17 @@ require('./src/registerCommands');
 
 client.login(client.config.dsc.token);
 
-client.on('ready', () => console.log('Ready !'));
+client.on('ready', () => {
+    console.log('Started refreshing application (/) commands');
+
+    const rest = new REST({ version: '9' }).setToken(client.config.dsc.token);
+
+    client.guilds.cache.map(async guild => {
+        await rest.put(
+            Routes.applicationGuildCommands(client.user.id, guild.id),
+            { body: client.commands.map(x => x) }
+        );
+    });
+
+    console.log('Successfully reloaded application (/) commands');
+});
